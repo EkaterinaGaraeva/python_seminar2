7487dc71cfeef8477d597c3cbb420bf903709195
Ваше имя <garaevaea@mail.ru>
2022-05-24 10:00:06 +0300

    Add Task003

--- Task003.py
+++ Task003.py
@@ -1,13 +1,13 @@
-# 3. Задайте список из n чисел последовательности (1+1.n)**n 
+# 3. Задайте список из n чисел последовательности (1+1/n)**n 
 # и выведите на экран их сумму.
 
 def sequence_of_numbers(number):
-    result = 0
+    result = []
     sum = 0
     for i in range(1, number):
-        result = (1 + 1 / i)**i
-        print(round(result, 3))
-        sum = sum + result
-    print(round(sum, 3))
+        result.append((1 + 1 / i)**i)
+        print(result[i-1])
+        sum += result[i-1]
+    print(f"Сумма чисел = {sum}")
 
-sequence_of_numbers(5)
+sequence_of_numbers(188)
