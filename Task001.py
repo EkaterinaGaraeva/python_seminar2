8d0f2826eb07cb0b6c99603981a2fb868c742f2a
Ваше имя <garaevaea@mail.ru>
2022-05-24 11:02:33 +0300

    Changed Task001, Task002

--- Task001.py
+++ Task001.py
@@ -4,7 +4,21 @@
 # seconds = int(time.time()*1000)
 # print(seconds % 100)
 
+# Вариант 1
 from datetime import datetime
-dt = datetime.now().microsecond
-print(int(dt % 10 + 10)) # от 10 до 20
+def randomize(min, max):
+    rand = int(datetime.now().microsecond) % 100
+    rand = rand * (max - min)//100 + min
+    return rand
 
+print(randomize(10, 20))
+
+# Вариант 2
+import time
+def new_random(min, max):
+    rnd = time.time()
+    rnd = rnd - int(rnd)
+    rnd = int(rnd * (max - min)) + min
+    return rnd
+
+print(new_random(50, 60))
--- Task002.py
+++ Task002.py
@@ -5,3 +5,25 @@ import random
 list_of_numbers = [1, 2, 3, 4]
 random.shuffle(list_of_numbers)
 print(list_of_numbers)
+
+
+# Вариант 2
+
+import time
+def new_random(min, max):
+    rnd = time.time()
+    rnd = rnd - int(rnd)
+    rnd = int(rnd * (max - min)) + min
+    return rnd
+
+spisok = [ 1, 4, 98, 'sddsd', True, 'rrr', False]
+size = len(spisok)
+print(size)
+new_spisok = []
+print(spisok)
+while size > 0:
+    index = new_random(0, size)
+    new_spisok.append(spisok[index])
+    spisok.pop(index)
+    size -= 1
+print(new_spisok)
