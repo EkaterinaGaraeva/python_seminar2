2514f03000b3fea5267dd5beb3dbfa66a812f1be
Ваше имя <garaevaea@mail.ru>
2022-05-24 08:29:24 +0300

    Changed Task001

--- Task001.py
+++ Task001.py
@@ -1,7 +1,10 @@
 # 1. Реализовать алгоритм задания случайного числа без генератора случайных чисел
 
-import time
+# import time
+# seconds = int(time.time()*1000)
+# print(seconds % 100)
 
-seconds = time.time()
-print(int(seconds) % 100)
+from datetime import datetime
+dt = datetime.now().microsecond
+print(int(dt % 10 + 10)) # от 10 до 20
 
